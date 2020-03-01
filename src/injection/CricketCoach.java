2ff1a179123cbd5da7ae6836e9d67f35236a1b4d
krishna sandeep <vanamasandeep2019@gmail.com>
2020-03-01 18:02:18 +0530

    Injecting  values

--- src/injection/CricketCoach.java
+++ src/injection/CricketCoach.java
@@ -4,6 +4,10 @@ public class CricketCoach implements Coach{
 	
 	private FortuneService fortuneService;
 	
+	private String emailAddress;
+	
+	private String team;
+	
 	public CricketCoach(){
 		
 	}
@@ -17,9 +21,32 @@ public class CricketCoach implements Coach{
 	public String getDailyWorkout() {
 		return "Cricket workout";
 	}
+	
+	
+	public void setTeam(String team) {
+		System.out.println("Setting Team");
+		this.team = team;
+		System.out.println("Email address="+team);
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Setting email address");
+		this.emailAddress = emailAddress;
+		System.out.println("Email address="+emailAddress);
+	}
+	
+	public String getEmail() {
+		System.out.println("Setting Email");
+		return emailAddress;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
 
 	@Override
 	public String getFortuneService() {
+		System.out.println("Iam inside Setter method");
 		return fortuneService.getFortune();
 	}
 
